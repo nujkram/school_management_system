dc785ec81707d221d186571fa4954ae92c2899b8
Mark <markjungersaniva@gmail.com>
2021-09-20 19:03:58 +0800

    Updated admin user form

--- admin_dashboard/controllers/views/admin_dashboard/accounts/forms.py
+++ admin_dashboard/controllers/views/admin_dashboard/accounts/forms.py
@@ -1,13 +1,13 @@
 from django import forms
 from accounts.models.account.models import Account
+from django.contrib.auth.forms import UserCreationForm
 
 
-class AccountForm(forms.ModelForm):
+class AccountRegisterForm(UserCreationForm):
     class Meta:
         model = Account
         fields = (
-            'email',
-            'username',
-            'user_type',
-
+            "username",
+            "email",
+            "user_type"
         )
