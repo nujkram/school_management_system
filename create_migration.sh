8985424acc5254e39e647f684b54f226ed5d35a0
Mark <markjungersaniva@gmail.com>
2021-09-20 18:49:01 +0800

    Added create migration script

---
+++ create_migration.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+mkdir -p accounts/migrations && touch accounts/migrations/__init__.py
+mkdir -p datetimedim/migrations && touch datetimedim/migrations/__init__.py
+mkdir -p profiles/migrations && touch profiles/migrations/__init__.py
+mkdir -p locations/migrations && touch locations/migrations/__init__.py
