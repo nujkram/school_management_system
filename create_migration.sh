7e7f15e08aad4f47d7c2d713202afa4fca621a2d
Mark <markjungersaniva@gmail.com>
2021-09-24 08:02:40 +0800

    Updated create migration

--- create_migration.sh
+++ create_migration.sh
@@ -4,3 +4,6 @@ mkdir -p accounts/migrations && touch accounts/migrations/__init__.py
 mkdir -p datetimedim/migrations && touch datetimedim/migrations/__init__.py
 mkdir -p profiles/migrations && touch profiles/migrations/__init__.py
 mkdir -p locations/migrations && touch locations/migrations/__init__.py
+mkdir -p locations/migrations && touch departments/migrations/__init__.py
+mkdir -p locations/migrations && touch courses/migrations/__init__.py
+mkdir -p locations/migrations && touch subjects/migrations/__init__.py
