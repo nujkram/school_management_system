cfd698f02e275461288541875451da752d8c0796
Mark <markjungersaniva@gmail.com>
2021-09-29 09:50:22 +0800

    Fixed create migration

--- create_migration.sh
+++ create_migration.sh
@@ -4,6 +4,6 @@ mkdir -p accounts/migrations && touch accounts/migrations/__init__.py
 mkdir -p datetimedim/migrations && touch datetimedim/migrations/__init__.py
 mkdir -p profiles/migrations && touch profiles/migrations/__init__.py
 mkdir -p locations/migrations && touch locations/migrations/__init__.py
-mkdir -p locations/migrations && touch departments/migrations/__init__.py
-mkdir -p locations/migrations && touch courses/migrations/__init__.py
-mkdir -p locations/migrations && touch subjects/migrations/__init__.py
+mkdir -p departments/migrations && touch departments/migrations/__init__.py
+mkdir -p courses/migrations && touch courses/migrations/__init__.py
+mkdir -p subjects/migrations && touch subjects/migrations/__init__.py
